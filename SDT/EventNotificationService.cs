4319758791ff1644159a458f4915c4760aadd8e5
ArtemB704 <iartembay04@gmail.com>
2025-01-16 11:33:33 +0200

    git add .

---
+++ SDT/EventNotificationService.cs
@@ -0,0 +1,21 @@
+﻿public class EventNotificationService
+{
+    private readonly IEventRepository _eventRepository;
+
+    public EventNotificationService(IEventRepository eventRepository)
+    {
+        _eventRepository = eventRepository;
+    }
+
+    public List<Event> GetCriticalEvents()
+    {
+        var allEvents = _eventRepository.GetAllEvents();
+        return allEvents.Where(e => e.Name.Contains("Критичний")).ToList();
+    }
+
+    public string NotifyCitizens(Event criticalEvent)
+    {
+        // Реалізація логіки надсилання повідомлень
+        return $"Оповіщення: {criticalEvent.Name} у місці {criticalEvent.Location}";
+    }
+}
---
+++ SDT/EventNotificationServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using Moq;
+using Xunit;
+using System.Collections.Generic;
+
+public class EventNotificationServiceTests
+{
+    [Fact]
+    public void GetCriticalEvents_ShouldReturnOnlyCriticalEvents()
+    {
+        // Arrange
+        var mockRepository = new Mock<IEventRepository>();
+        mockRepository.Setup(repo => repo.GetAllEvents()).Returns(new List<Event>
+        {
+            new Event { Id = 1, Name = "Критичний: Пожежа", Location = "Київ" },
+            new Event { Id = 2, Name = "Інформаційний: Дощ", Location = "Львів" }
+        });
+
+        var service = new EventNotificationService(mockRepository.Object);
+
+        // Act
+        var criticalEvents = service.GetCriticalEvents();
+
+        // Assert
+        Assert.Single(criticalEvents);
+        Assert.Equal("Критичний: Пожежа", criticalEvents[0].Name);
+    }
+
+    [Fact]
+    public void NotifyCitizens_ShouldReturnNotificationMessage()
+    {
+        // Arrange
+        var criticalEvent = new Event { Id = 1, Name = "Критичний: Повінь", Location = "Одеса" };
+        var service = new EventNotificationService(null);
+
+        // Act
+        var message = service.NotifyCitizens(criticalEvent);
+
+        // Assert
+        Assert.Equal("Оповіщення: Критичний: Повінь у місці Одеса", message);
+    }
+}
